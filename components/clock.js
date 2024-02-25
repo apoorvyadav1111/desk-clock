32657da50f3946d43f8a77e637c2ed74a297e562
ayadav16 <ayadav7@binghamton.edu>
2024-02-25 12:51:36 -0500

    edit colors

--- components/clock.js
+++ components/clock.js
@@ -11,17 +11,22 @@ const Clock = () => {
     return ()=>clearInterval(interval);
     }, []);
 
-    const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+    let options = { weekday: 'short'};
+    const formattedday = time.toLocaleDateString(undefined, options);
+    options = { year: 'numeric', month: 'short', day: 'numeric' };
     const formattedDate = time.toLocaleDateString(undefined, options);
   
     return (
       <div className="flex flex-col h-screen items-center justify-center">
         <div className="text-start">
-          <div className="text-5xl dark:text-gray-400">
-            <span>&nbsp;</span>{formattedDate}
+          <div className="text-5xl text-gray-700 dark:text-gray-400">
+            <span className="text-red-500 font-bold">&nbsp;{formattedday}, </span>{formattedDate}
           </div>
           <div className="translate-y-[-4] text-9xl">
-            {time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit'})}
+            {time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}
+          {/* </div>
+          <div className="text-orange-500 text-3xl"> */}
+            {<span className="text-5xl text-orange-500">:{time.toLocaleTimeString([],{second: '2-digit' })>9?time.toLocaleTimeString([],{second: '2-digit' }):'0'+time.toLocaleTimeString([],{second: '2-digit' })}</span> }
           </div>
 
         </div>
